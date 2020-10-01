f4d27b323e1f21efafede1d69e1702189d0f8c42
lucashalbert <lhalbert@lhalbert.xyz>
2020-10-01 16:02:29 -0400

    Deployment test

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,3 +1,35 @@
+#!/bin/bash
+docker_archs=(amd64 i386 arm32v6 arm32v7 arm64v8 ppc64le s390x)
+
+for docker_arch in amd64 arm32v6 arm64v8; do
+    # Push each of the images to Docker Hub
+    docker push ${repo}:${docker_arch}-${ver}
+
+    # Create and annotate arch/ver docker manifest
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create ${repo}:${docker_arch}-${ver} ${repo}:${docker_arch}-${ver}
+
+    # Annotate arch/ver docker manifest
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate ${repo}:${docker_arch}-${ver} ${repo}:${docker_arch}-${ver} --os linux --arch ${image_arch}
+
+    # Push version and architecture docker manifest
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push ${repo}:${docker_arch}-${ver}
+
+    # Generate Dynamic Manifest Image List
+    manifest_images="${manifest_images} ${repo}:${docker_arch}-${ver}"
+
+done
+
+# Create version specific docker manifest
+#DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/curl:${curl_ver} lucashalbert/curl:amd64-${curl_ver} lucashalbert/curl:arm32v6-${curl_ver} lucashalbert/curl:arm64v8-${curl_ver}
+DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create ${repo}:${ver} ${manifest_images}
+
+# Create latest docker manifest
+#DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/curl:latest lucashalbert/curl:amd64-${curl_ver} lucashalbert/curl:arm32v6-${curl_ver} lucashalbert/curl:arm64v8-${curl_ver}
+DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create ${repo}:latest ${manifest_images}
+
+
+
+
 for docker_arch in ${docker_archs[@]}; do
     case ${docker_arch} in
         i386 )    image_arch="i386"    ;;
@@ -9,11 +41,11 @@ for docker_arch in ${docker_archs[@]}; do
         s390x )   image_arch="s390x"   ;;   
     esac
 
-    # Push each of the images to Docker Hub
-	docker push ${repo}:${docker_arch}-${curl_ver}
+    # Annotate version specific docker manifest
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate ${repo}:${ver} ${repo}:${docker_arch}-${ver} --os linux --arch ${image_arch}
 
-	# Push version and architecture docker manifest
-	DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push ${repo}:${docker_arch}-${curl_ver}
+    # Annotate latest docker manifest
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate ${repo}:latest ${repo}:${docker_arch}-${curl_ver} --os linux --arch ${image_arch}
 
 	# Push version specific docker manifest
 	DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push ${repo}:${curl_ver}
