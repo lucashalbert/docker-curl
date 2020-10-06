25a82fdb20b349585c75bffb7b56f29928da8256
lucashalbert <lhalbert@lhalbert.xyz>
2020-10-06 13:27:45 -0400

    Update local build script for testing

--- build-local.sh
+++ build-local.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-curl_ver=${curl_ver:-$(curl -s "https://pkgs.alpinelinux.org/package/edge/main/x86_64/curl" | grep -A3 Version | grep href | sed 's/<[^>]*>//g' | tr -d " ")}
+ver=${ver:-$(curl -s "https://pkgs.alpinelinux.org/package/edge/main/x86_64/curl" | grep -A3 Version | grep href | sed 's/<[^>]*>//g' | tr -d " ")}
 build_date=${build_date:-$(date +"%Y%m%dT%H%M%S")}
 
-for docker_arch in amd64 arm32v6 arm64v8; do
+for docker_arch in ${docker_archs}; do
     case ${docker_arch} in
-        amd64   ) qemu_arch="x86_64" ;;
-        arm32v6 ) qemu_arch="arm" ;;
-        arm64v8 ) qemu_arch="aarch64" ;;    
+        i386    ) qemu_arch="i386"    image_arch="386"     variant=""   ;;
+        amd64   ) qemu_arch="x86_64"  image_arch="amd64"   variant=""   ;;
+        arm32v6 ) qemu_arch="arm"     image_arch="arm"     variant="v6" ;;
+        arm32v7 ) qemu_arch="arm"     image_arch="arm"     variant="v7" ;;
+        arm64v8 ) qemu_arch="aarch64" image_arch="arm64"   variant="v8" ;;
+        ppc64le ) qemu_arch="ppc64le" image_arch="ppc64le" variant=""   ;;
+        s390x   ) qemu_arch="s390x"   image_arch="s390x"   variant=""   ;;
     esac
     cp Dockerfile.cross Dockerfile.${docker_arch}
     sed -i "s|__BASEIMAGE_ARCH__|${docker_arch}|g" Dockerfile.${docker_arch}
     sed -i "s|__QEMU_ARCH__|${qemu_arch}|g" Dockerfile.${docker_arch}
-    sed -i "s|__CURL_VER__|${curl_ver}|g" Dockerfile.${docker_arch}
+    sed -i "s|__CURL_VER__|${ver}|g" Dockerfile.${docker_arch}
     sed -i "s|__BUILD_DATE__|${build_date}|g" Dockerfile.${docker_arch}
     if [ ${docker_arch} == 'amd64' ]; then
         sed -i "/__CROSS__/d" Dockerfile.${docker_arch}
@@ -31,10 +35,10 @@ for docker_arch in amd64 arm32v6 arm64v8; do
 
     # Build
     if [ "$EUID" -ne 0 ]; then
-        sudo docker build -f Dockerfile.${docker_arch} -t lucashalbert/docker-curl:${docker_arch} .
+        sudo docker build -f Dockerfile.${docker_arch} -t lucashalbert/curl:${docker_arch}-${ver} .
         #sudo docker push lucashalbert/docker-curl:${docker_arch}
     else
-        docker build -f Dockerfile.${docker_arch} -t lucashalbert/docker-curl:${docker_arch} .
+        docker build -f Dockerfile.${docker_arch} -t lucashalbert/curl:${docker_arch}-${ver} .
         #docker push lucashalbert/docker-curl:${docker_arch}
     fi
 done
